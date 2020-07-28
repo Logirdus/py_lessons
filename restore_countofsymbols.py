4b2fcc01184722055c81d4b8d4f3c251ae6c6539
Logirdus <regdust@yandex.ru>
2020-07-28 18:29:22 +0300

    Бета решение задачи Stepic 3.4 Файловый ввод/вывод
    Добавлена работа с файлом вывода. Нужно переделать к формату with open...

--- restore_countofsymbols.py
+++ restore_countofsymbols.py
@@ -3,17 +3,22 @@ count = ''
 #print(count + str(3))
 def counting(): pass
 
+restored = open('restored_file.txt', 'w')
+
 with open('text.txt', 'r') as quiz:
     for line in quiz:
         alfa = line[0]
         for i in range(1, len(line)):
             if line[i] not in numbers or i+1 > len(line):
                 print(alfa * int(count), end='')
+                restored.write(alfa * int(count))
                 alfa = line[i]
                 count = ''
             else:
                 count += str(line[i])
+        restored.write(alfa * int(count))
         print(alfa * int(count), end='')
             #print(line[i])
 
 
+restored.close()
