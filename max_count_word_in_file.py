34c4ad2afc42c441844ce4710e9eb553a41f4ce2
Logirdus <regdust@yandex.ru>
2020-11-09 15:59:40 +0300

    Нахождение максимального количества вхождений слова в файле оптимизировано с использованием функции set

--- max_count_word_in_file.py
+++ max_count_word_in_file.py
@@ -5,19 +5,11 @@ with open('input2.txt', 'r') as input_file:
         words = [i for i in line.lower().split()]
         for word in words:
             d.append(word)
-    d.sort()
-    print(d)
-    print(d.count('abv'))
     maxd = d[0]
-    i = 0
-    while i < len(d) - 1:
-        i += 1
-        if d[i] == maxd:
-            continue
-        else:
-            if d.count(maxd) < d.count(d[i]):
-                maxd = d[i]
-    print(maxd, d.count(maxd))
+    for word in sorted(set(d)):
+        if d.count(word) > d.count(maxd):
+            maxd = word
+print(maxd, d.count(maxd))
 
 
 '''
