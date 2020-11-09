e82dfba69b3393a144d5a9874dc794624f571ed0
Logirdus <regdust@yandex.ru>
2020-11-09 16:23:28 +0300

    Нахождение максимального количества вхождений слова в файле оптимизировано с использованием функции set Добавлено описание алгоритма и новый файл с входными значениями

--- max_count_word_in_file.py
+++ max_count_word_in_file.py
@@ -5,10 +5,10 @@ with open('input2.txt', 'r') as input_file:
         words = [i for i in line.lower().split()]
         for word in words:
             d.append(word)
-    maxd = d[0]
-    for word in sorted(set(d)):
-        if d.count(word) > d.count(maxd):
-            maxd = word
+maxd = d[0]
+for word in sorted(set(d)):
+    if d.count(word) > d.count(maxd):
+        maxd = word
 print(maxd, d.count(maxd))
 
 
@@ -20,4 +20,10 @@ print(maxd, d.count(maxd))
     если они одинаковы, continue
     если не одинаковы
     сравнить их количество - взять за макс
+Оптимизировано:
+    Делаем список  уникальных значений из исходного списка set(d)
+    Чтобы получить сортированный список делаем sorted(set(d))
+    признаем самым частым первое значение списка
+    Для каждого значения из set считаем его количество в исходном списке d
+    если количество больше, чем у первого - берем его за самый частый
 '''
